962d5fa57192d99f1c0d0286ec4fecb5e13b7f96
Joshua Vedane <jvedane@gmail.com>
2021-01-13 16:14:57 -0600

    basemode completed

---
+++ src/components/App/App.jsx
@@ -0,0 +1,78 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Header from '../Header/Header.jsx'
+import Footer from '../Footer/Footer.jsx'
+import GuestForm from '../GuestForm/GuestForm.jsx'
+import GuestList from '../GuestList/GuestList.jsx'
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies.jsx'
+import PartyLeader from '../PartyLeader/PartyLeader.jsx'
+import './App.css';
+
+function App() {
+  let [guestList, setGuestList] = useState([]);
+  let [newGuestName, setNewGuestName] = useState('');
+  let [newGuestMeal, setNewGuestMeal] = useState('false');
+
+  //On load, get guests
+  useEffect(() => {
+    getGuests()
+  }, [])
+
+  const getGuests = () => {
+    axios.get('/guests')
+      .then(response => {
+        setGuestList(response.data)
+      })
+      .catch(err => {
+        alert('error getting guests');
+        console.log(err);
+      })
+  }
+
+
+  const addGuest = () => {
+    axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
+      .then(response => {
+        // clear inputs
+        setNewGuestName('');
+        setNewGuestMeal(false);
+
+        getGuests();
+      })
+      .catch(err => {
+        alert('Error Adding Guest');
+        console.log(err);
+      })
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newGuestName) {
+      addGuest();
+    }
+    else {
+      alert('The new guest needs a name!');
+    }
+  }
+
+  console.log(newGuestMeal)
+  return (
+    <div className="App">
+      <Header />
+      <PartyLeader leader={guestList[0]}/>
+      <GuestForm
+        newGuestName={newGuestName}
+        setNewGuestName={setNewGuestName}
+        newGuestMeal={newGuestMeal}
+        setNewGuestMeal={setNewGuestMeal}
+        handleSubmit={handleSubmit}
+      />
+      <GuestList guestList={guestList} />
+      <DinnerSupplies guestList={guestList} />
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
